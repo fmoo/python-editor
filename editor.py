cca35992ffe9d2c4346c14d14177a3fe34332b37
Peter Ruibal <ruibalp@gmail.com>
2016-11-16 09:55:04 -0800

    Version bump (1.0.2)

--- editor.py
+++ editor.py
@@ -17,7 +17,7 @@ __all__ = [
     'EditorError',
 ]
 
-__version__ = '0.4'
+__version__ = '1.0.2'
 
 
 class EditorError(RuntimeError):
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__VERSION__ = '1.0.1'
+__VERSION__ = '1.0.2'
 
 from setuptools import setup
 try:
